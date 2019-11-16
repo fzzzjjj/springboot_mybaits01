23f29ecc18cca56bc0b90d17845d28cae240e47c
fzzzjjj <51238439+fzzzjjj@users.noreply.github.com>
2019-11-16 23:49:51 +0800

    Initial commit

---
+++ src/main/java/com/example/springboot_mybaits01/pojo/Info.java
@@ -0,0 +1,53 @@
+package com.example.springboot_mybaits01.pojo;
+
+public class Info {
+    private Integer id;
+
+    private String name;
+
+    private String sex;
+
+    private String classname;
+
+    private Integer age;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex == null ? null : sex.trim();
+    }
+
+    public String getClassname() {
+        return classname;
+    }
+
+    public void setClassname(String classname) {
+        this.classname = classname == null ? null : classname.trim();
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
---
+++ src/test/java/com/example/springboot_mybaits01/SpringbootMybaits01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_mybaits01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMybaits01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
